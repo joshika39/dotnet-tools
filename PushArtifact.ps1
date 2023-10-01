77ddd7270320654d57299c13fa139effdb4801e9
Joshua Hegedus <josh.hegedus@outlook.com>
2023-10-01 20:32:29 +0200

    fixed output

--- PushArtifact.ps1
+++ PushArtifact.ps1
@@ -16,7 +16,7 @@ $version = $Version
 $versionDir = "v$version"
 
 if (-not (Test-Path $EnvFile )) {
-    Write-Host "The .env file does not exist."
+    Write-Output "The .env file does not exist."
     exit 1
 }
 
@@ -26,19 +26,19 @@ get-content $EnvFile | ForEach-Object {
 }
 
 if ($null -eq $env:WORK_DIR -or $null -eq $env:NUSPEC_DIR -or $null -eq $env:BUILD_CONFIG) {
-    Write-Host "Missing required variables."
+    Write-Output "Missing required variables."
     exit 1
 }
 
 $nugetExePath = "$env:WORK_DIR\nuget.exe"
 
 if ( -not (Test-Path $nugetExePath)) {
-    Write-Host "Missing nuget.exe file."
+    Write-Output "Missing nuget.exe file."
     exit 1
 }
 
 if ( -not (Test-Path $env:NUSPEC_DIR\.projects)) {
-    Write-Host "Missing projects file."
+    Write-Output "Missing projects file."
     exit 1
 }
 
@@ -46,13 +46,13 @@ $projects = Get-Content $env:NUSPEC_DIR\.projects
 
 foreach ($project in $projects) {
     New-Item -ItemType Directory -Path $env:NUSPEC_DIR\tmp
-    Write-Host "Created temp folder: $env:NUSPEC_DIR\tmp"
+    Write-Output "Created temp folder: $env:NUSPEC_DIR\tmp"
 
     Copy-Item -Path $env:NUSPEC_DIR\Projects\$project.nuspec -Destination $env:NUSPEC_DIR\tmp
-    Write-Host "Copied: $env:NUSPEC_DIR\Projects\$project.nuspec -> $env:NUSPEC_DIR\tmp\$project.nuspec"
+    Write-Output "Copied: $env:NUSPEC_DIR\Projects\$project.nuspec -> $env:NUSPEC_DIR\tmp\$project.nuspec"
     ((Get-Content -path $env:NUSPEC_DIR\tmp\$project.nuspec -Raw) -replace '{CONFIGURATION}', $env:BUILD_CONFIG) | Set-Content -Path $env:NUSPEC_DIR\tmp\$project.nuspec
     
-    Write-Host "$project.nuspec set up with $env:BUILD_CONFIG build configuration"
+    Write-Output "$project.nuspec set up with $env:BUILD_CONFIG build configuration"
     & $nugetExePath pack $env:NUSPEC_DIR\tmp\$project.nuspec -version $version -OutputDirectory $env:NUSPEC_DIR\Packages\$versionDir\
 }
 
